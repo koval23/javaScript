ce7f1af6a87d505d1d3b420c93757f0ac8d10e7c
yura <ukoval400@gmail.com>
2022-08-21 11:10:01 +0300

    metods

---
+++ js/js/lessons.js
@@ -0,0 +1,33 @@
+/*
+
+* ============== примери масивов ==============
+
+
+let numbers = [1, 4, 5, 22, 7, 6, 25, 4,];
+
+let users = [
+	{ name: 'Pasha', age: 17 },
+	{ name: 'Roma', age: 25 },
+	{ name: 'Kola', age: 16 },
+	{ name: 'Vana', age: 28 },
+]
+
+*/
+
+//=========== REDUCE ===============
+
+//let a = [1, 4, 5, 8, 7];
+//let w = [
+//	{ name: 'Pasha', age: 13 },
+//	{ name: 'Kola', age: 27 },
+//	{ name: 'Vana', age: 17 },
+//	{ name: 'Vova', age: 24 },
+//]
+
+//let b = w.reduce((accum, item) => {
+//	accum.push(item.age);
+//	return accum;
+//}, []);
+
+//console.log('=============');
+//console.log(b);
