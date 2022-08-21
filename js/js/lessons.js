138b547f906edaa6b9e9bf88e3f1aeca452d552e
yura <ukoval400@gmail.com>
2022-08-21 16:43:28 +0300

    add metodsArray

--- js/js/lessons.js
+++ js/js/lessons.js
@@ -5,6 +5,8 @@
 
 let numbers = [1, 4, 5, 22, 7, 6, 25, 4,];
 
+let arrInArray = [[1,2,3],[4,5,6],[7,8,9]]
+
 let users = [
 	{ name: 'Pasha', age: 17 },
 	{ name: 'Roma', age: 25 },
@@ -16,7 +18,42 @@ let users = [
 
 //=========== REDUCE ===============
 
-//let a = [1, 4, 5, 8, 7];
+let a = [2, 4, 3, 5, 8, 22, 7, 3];
+//console.log(a);
+
+//let b = a.reduce((accum, item, index) => {
+
+//	console.log(accum);
+
+//	if (item > accum[1]) {
+
+//		return [index, item]; // accum = [index, item]
+
+//	}
+//	return accum
+
+//}, [0, a[0]])
+
+//console.log(b);
+let arrInArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+let c = arrInArray.reduce((accum, item) => {
+
+	return accum.concat(item)
+
+}, [])
+
+console.log(c);
+
+
+
+
+
+
+
+
+
+
 //let w = [
 //	{ name: 'Pasha', age: 13 },
 //	{ name: 'Kola', age: 27 },
@@ -24,10 +61,77 @@ let users = [
 //	{ name: 'Vova', age: 24 },
 //]
 
-//let b = w.reduce((accum, item) => {
-//	accum.push(item.age);
-//	return accum;
-//}, []);
+
+
+//let b = a.reduce((accum, item) => {
+//	console.log(accum);
+//	return accum + item
+
+//}, 0)
+//console.log(b);
+
+
+
+//let sum = 0
+//for (let i = 0; i < a.length; i++) {
+//	sum += a[i]
+//}
+//console.log(sum);
+
+//let max = a[0]
+//for (let i = 0; i < a.length; i++) {
+//	console.log(a[i]);
+//	if (a[i] > max) {
+//console.log(a[i]);
+//max = a[i]
+//console.log(max);
+//	}
+//}
+//console.log(max);
+
+//let b = a.reduce((accum, item) => {
+//	if (accum < item) {
+//		console.log(accum);
+//		return item
+//	} else {
+//		return accum
+//	}
+
+//})
+//console.log(b);
+//===============================
+//let accum = [0, [a[0]]]
+
+//let b = a.reduce((accum, item, index) => {
+//	if (item > accum[1]) {
+//		return [index, item];
+//	} else {
+//		return accum
+//	}
+//}, [0, a[0]])
+//console.log(b);
+
+
+//=============================
+
+
+// до старта reduce равно нулевой елемент масива
+// если мы ек укажем сами
+
+
+//let arrInArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+//console.log(arrInArray);
+
+//let newArr = arrInArray.reduce((accum, item) => {
+
+//	return accum.concat(item);
+//}, [])
+
+//console.log(newArr);
+
+
+
+
 
 //console.log('=============');
 //console.log(b);
